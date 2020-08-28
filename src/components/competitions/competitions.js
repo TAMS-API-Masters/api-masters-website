55c0719a815b4ccbb44c3b59c2107f8611593b24
natesabrown <nateabrown@icloud.com>
2020-08-28 14:11:57 -0500

    make facebook things invisible

--- src/components/competitions/competitions.js
+++ src/components/competitions/competitions.js
@@ -1,18 +1,21 @@
 import facebook from './facebook.png'
 
-export default [
-    {
-        title: "Minithon #1",
-        subtitle: "Facebook Foolery",
-        description: "Be sure to check out our very first minithon: Facebook Foolery! We’ll be sure to have a lot of cool APIs for you to work with and a lot of resources to help you along the way. Make sure you sign up to get first dibs on your team members and other cool swag!",
-        image: facebook,
-        api_name: "Facebook Graph API"
-    },
-    {
-        title: "Minithon #1",
-        subtitle: "Facebook Foolery",
-        description: "Be sure to check out our very first minithon: Facebook Foolery! We’ll be sure to have a lot of cool APIs for you to work with and a lot of resources to help you along the way. Make sure you sign up to get first dibs on your team members and other cool swag!",
-        image: facebook,
-        api_name: "Facebook Graph API"
-    }
-]
+// EXAMPLE DATA
+// export default [
+//     {
+//         title: "Minithon #1",
+//         subtitle: "Facebook Foolery",
+//         description: "Be sure to check out our very first minithon: Facebook Foolery! We’ll be sure to have a lot of cool APIs for you to work with and a lot of resources to help you along the way. Make sure you sign up to get first dibs on your team members and other cool swag!",
+//         image: facebook,
+//         api_name: "Facebook Graph API"
+//     },
+//     {
+//         title: "Minithon #1",
+//         subtitle: "Facebook Foolery",
+//         description: "Be sure to check out our very first minithon: Facebook Foolery! We’ll be sure to have a lot of cool APIs for you to work with and a lot of resources to help you along the way. Make sure you sign up to get first dibs on your team members and other cool swag!",
+//         image: facebook,
+//         api_name: "Facebook Graph API"
+//     }
+// ]
+
+export default []
